de983bfe17f409419e0a15095916351edd982cd4
Matt McGee <mlmcgeenc@gmail.com>
2022-05-19 10:17:48 -0400

    build 5 day forecast from search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var myNum = "4d1d3a785c5d9d29d73debb3f37fbb0c";
-var conditionsUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
 var form = document.getElementById("search");
+var weatherConditions = {};
 
 var fetchData = function (cityName) {
 	var locationUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${myNum}`;
@@ -23,6 +23,7 @@ var fetchData = function (cityName) {
 				.then(function (weatherConditions) {
 					console.log(weatherConditions);
 					setCurrentConditionsDisplay(weatherConditions);
+					buildFiveDayForecast(weatherConditions);
 				});
 		})
 		.catch(function () {
@@ -37,25 +38,45 @@ var setCurrentConditionsDisplay = function (weatherConditions) {
 	document.getElementById("current-temp").textContent = weatherConditions.current.temp;
 	document.getElementById("current-wind").textContent = weatherConditions.current.wind_speed;
 	document.getElementById("current-humidity").textContent = weatherConditions.current.humidity;
+	// TODO Color code UV index
 	document.getElementById("current-uv-index").textContent = weatherConditions.current.uvi;
 };
 
-// TODO Finish building card and write loop to iterate over 5 day forecast, adding cards to #five-day-forecast
-var createDailyCard = function(cardIndex) {
-    var newCard = document.createElement('div')
-    newCard.classList.add("card", "col-12", "col-lg-5", "col-xl", "small", "m-2", "p-2")
-
-    var cardTitle = document.createElement('div')
-    cardTitle.setAttribute("class", "card-title")
-    cardTitle.textContent = "FUTURE DATE"
-
-    var cardIcon = 
-}
-
 var handleSearch = function (e) {
 	e.preventDefault();
-	console.log(e.target[0].value);
 	fetchData(e.target[0].value);
+	e.target[0].value = "";
+};
+
+var createDailyCard = function (cardIndex, weatherConditions) {
+	var newCard = document.createElement("div");
+	newCard.classList.add("card", "col-12", "col-lg-5", "col-xl", "small", "m-2", "p-2");
+
+	var cardTitle = document.createElement("div");
+	cardTitle.setAttribute("class", "card-title");
+	cardTitle.textContent = moment().add(cardIndex, "days").format(" (M / D / YYYY) ");
+
+	var cardIcon = document.createElement("img");
+	cardIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherConditions.daily[cardIndex].weather[0].icon + ".png");
+	cardIcon.setAttribute("style", "max-width: 4rem;");
+
+	var cardTemp = document.createElement("p");
+	cardTemp.textContent = "Temp: " + weatherConditions.daily[cardIndex].temp.day + " °F";
+
+	var cardWindSpeed = document.createElement("p");
+	cardWindSpeed.textContent = "Wind: " + weatherConditions.daily[cardIndex].wind_speed + " mph";
+
+	var cardHumidity = document.createElement("p");
+	cardHumidity.textContent = "Humidity: " + weatherConditions.daily[cardIndex].humidity + " %";
+
+	newCard.append(cardTitle, cardIcon, cardTemp, cardWindSpeed, cardHumidity);
+	document.getElementById("five-day-forecast").appendChild(newCard);
+};
+
+var buildFiveDayForecast = function (weatherConditions) {
+	for (i = 1; i < 6; i++) {
+		createDailyCard(i, weatherConditions);
+	}
 };
 
 form.addEventListener("submit", handleSearch);
